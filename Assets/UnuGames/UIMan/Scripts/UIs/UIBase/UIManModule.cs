5fab371c7f06c340609144efd5d5a036384ef4d4
minhdu <cp.dev.minhdu@gmail.com>
2016-10-19 20:28:35 +0700

    add UIManModule

---
+++ Assets/UnuGames/UIMan/Scripts/UIs/UIBase/UIManModule.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UnuGames
+{
+	public class UIManModule<T> : ViewModelBehaviour, IModule
+	{
+
+		T _dataInstance = default(T);
+
+		[UIManProperty]
+		public virtual T DataInstance {
+			get {
+				return _dataInstance;
+			}
+			set {
+				_dataInstance = value;
+				NotifyModelChange (DataInstance);
+			}
+		}
+
+		public object OriginalData {
+			get {
+				return (object)DataInstance;
+			}
+			set {
+				DataInstance = (T)value;
+			}
+		}
+	}
+}
