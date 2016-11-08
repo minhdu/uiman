e4f6a2973f985ba0aa863fc730dc3e711b4b70c2
minhdu <cp.dev.minhdu@gmail.com>
2016-11-08 22:40:08 +0700

    add BinberTemplate

---
+++ Assets/UnuGames/UIMan/Scripts/MVVM/Binders/BinberTemplate.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnuGames.MVVM
+{
+	[DisallowMultipleComponent]
+	public class BinderTemplate : BinderBase
+	{
+
+		[HideInInspector]
+		public BindingField yourValue = new BindingField ("Text");
+		//Define any field for binding as you want, just copy above fied
+
+		public override void Init (bool forceInit)
+		{
+			if (CheckInit (forceInit)) {
+				// Get view's components here
+
+				SubscribeOnChangedEvent (yourValue, OnUpdateValue);
+			}
+		}
+
+		public void OnUpdateValue (object newValue)
+		{
+			if (newValue == null) {
+				// Do what you want for null value
+				return;
+			}
+
+			// Cast newValue into your binding type and assign to view components
+		}
+
+		public override void OnDisable ()
+		{
+			throw new System.NotImplementedException ();
+		}
+	}
+}
