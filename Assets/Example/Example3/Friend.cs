b53a94dc82331dce150444aa09f7be17d16c6f0f
minhdu <cp.dev.minhdu@gmail.com>
2017-02-01 14:04:55 +0700

    remove source code, prepare for sale...!

--- Assets/Example/Example3/Friend.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class Friend {
-
-	public string Name;
-	public int Age;
-}
--- Assets/Example/Example7/UserModule.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-using UnuGames;
-
-public class UserModule : UIManModule<UserModel> {
-
-}
