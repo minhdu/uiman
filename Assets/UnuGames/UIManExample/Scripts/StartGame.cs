53a3fa640b65cc1b6c07c3ddd67a9d49c04fd6fd
minhdu <cp.dev.minhdu@gmail.com>
2018-07-20 21:26:22 +0700

    remove unused code

--- Assets/UnuGames/UIManExample/Scripts/StartGame.cs
+++ Assets/UnuGames/UIManExample/Scripts/StartGame.cs
@@ -6,7 +6,6 @@ public class StartGame : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		UIMan.Instance.Preload<UIMainMenu> ();
 		UIMan.Instance.ShowScreen<UIMainMenu> ();
 	}
 	
